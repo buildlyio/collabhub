8ea1bde2e6877dd2376c87ae66f45ebf949d6d42
glind <greg@buildly.io>
2023-09-08 11:31:00 -0700

    prod

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -13,7 +13,7 @@ DATABASES = {
     }
 }
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bugbounty-gftbt.ondigitalocean.app', '127.0.0.1', '[::1]','bounty.buildly.io','www.bugbounty.io','market.buildly.io']
 
@@ -22,29 +22,6 @@ try:
 except ImportError:
     pass
 
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://6975369468324c3baa02b389ee8dcbeb@o1380602.ingest.sentry.io/6693855",
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-
-    # By default the SDK will try to use the SENTRY_RELEASE
-    # environment variable, or infer a git commit
-    # SHA as release, however you may want to set
-    # something more human-readable.
-    # release="myapp@1.0.0",
-)
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  # new
 DEFAULT_FROM_EMAIL = "team@buildly.io"
