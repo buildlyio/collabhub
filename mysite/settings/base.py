43daabcbb8925ffc571be2d0fb5f9beef13e750f
glind <greg@buildly.io>
2023-09-11 09:20:26 -0700

    fix auth setup

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -75,7 +75,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 
