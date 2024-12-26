5f50cd675dd1a849845ee190cf71717e777fb66d
Greg Lind <gwlind@gmail.com>
2024-12-26 07:57:20 -0800

    choices

--- onboarding/forms.py
+++ onboarding/forms.py
@@ -4,9 +4,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import TeamMember
+from .models import TEAM_MEMBER_TYPES
 
 class TeamMemberRegistrationForm(UserCreationForm):
-    team_member_type = forms.ChoiceField(choices=TeamMember.TEAM_MEMBER_TYPES)
+    team_member_type = forms.ChoiceField(choices=TEAM_MEMBER_TYPES)
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField()
