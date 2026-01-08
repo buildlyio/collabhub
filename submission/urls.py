161cc7c2275c3856b54fa083d24032e5887e5673
Greg Lind <gwlind@gmail.com>
2026-01-08 15:12:53 -0800

    fix app name

--- submission/urls.py
+++ submission/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'submission'
+
 urlpatterns = [
     path('generate/', views.generate_link, name='generate_link'),
     path('submit/<str:unique_url>/', views.submission_form, name='submission_form'),
